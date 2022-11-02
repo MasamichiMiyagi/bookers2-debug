26d3cc59315245e51e9be761609b892442d50958
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-11-02 04:08:31 +0000

    [2022/11/2/13:08]favorites

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,12 @@ class Book < ApplicationRecord
   validates :body,presence:true,length:{maximum:200}
 
   belongs_to :user
+  has_many :favorites, dependent: :destroy
 
   has_one_attached :profile_image
+
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
