4fac889f8613794b1a7180ba82d21432e62fb950
MasamichiMiyagi <114850152+MasamichiMiyagi@users.noreply.github.com>
2022-11-10 04:49:02 +0000

    [2022/11/10/13:48]js

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     #リダイレクト先を削除（create.js.erbファイルを参照するようになる）
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     #リダイレクト先を削除（destroy.js.erbファイルを参照するようになる）
   end
